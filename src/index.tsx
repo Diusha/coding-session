58b0e6e41ff540c49f7c96dd8deb30e67fd2a194
andrei <a.taranuta@therapanacea.eu>
2022-03-23 12:46:26 +0200

    basic setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import App from "./App";
 import theme from "./theme";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.querySelector("#root")
 );
