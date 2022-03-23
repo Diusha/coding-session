50d4d55b2028ea761ddb96e5784223dd5f6ce41b
andrei <a.taranuta@therapanacea.eu>
2022-03-23 11:40:21 +0200

    redux and rtk install

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
