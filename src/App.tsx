d0c94f03a17b43880661661fb47a0e1b69faee1e
Andrei <diusha13@gmail.com>
2022-03-23 11:26:10 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@mui/material";
+import "./styles.css";
+import s from "./styles.module.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Card>So</Card>
+      ssdsd
+      <h1>Hello dsdCodeSandbox</h1>
+      <h2 className={s.test}>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
